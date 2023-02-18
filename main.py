31d1ff4cb8239653a8603cdad49904143b504a64
Kārlis Rūdolfs Birznieks <125714170+KarlisRudolfsBirznieks221RDB412@users.noreply.github.com>
2023-02-18 13:31:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,29 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next_char in enumerate(text):
-        if next_char in "([{":
-            opening_brackets_stack.append(Bracket(next_char, i))
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i+1))
             
-        elif next_char in ")]}":
-            if not opening_brackets_stack:
-                return i + 1
+      if next in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                print(i + 1)
+                return
+            opening_brackets_stack.pop()
                 
-            pedejais = opening_brackets_stack.pop()
-            if not are_matching(pedejais.char, next_char):
-                return i + 1
-                
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
-    return "Success"
+       if not opening_brackets_stack:
+        print("Success")
+     else:
+         print(opening_brackets_stack[-1].position)
+       return
+
+      
 
 def main():
     text = input()
+    if text[0] != 'I'
+    return
+
     mismatch = find_mismatch(text)
     print(mismatch)
 
